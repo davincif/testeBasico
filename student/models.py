03cc240a0525f51b158036616752838e2d731b54
davincif <ldavincif@gmail.com>
2021-07-12 17:34:39 -0300

    feat: static files added

--- student/models.py
+++ student/models.py
@@ -5,5 +5,5 @@ from teacher.models import Teacher
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, blank=False, null=False)
     name = models.CharField(max_length=150, blank=False, null=False)
--- teacher/models.py
+++ teacher/models.py
@@ -5,5 +5,5 @@ from classroom.models import Classroom
 
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=150, blank=False, null=False)
